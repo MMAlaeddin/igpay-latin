54b51c418a7079edccdd15e9d6e0bfef8124ba53
Student <example@example.com>
2020-01-23 13:29:48 -0800

    add else if to split words beginning with constanants

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,22 @@ $(document).ready(function(){
 		var vowels = ["a", "e", "i", "o", "u"];
 			vowels.forEach(function(vowel) {
 			if (userInput.charAt(0) === vowel) {
-			 $("#output").text(userInput + "way");
+			 $("#output").text(userInput + "way");	
+		
+			} else if (userInput.charAt(0) !== vowel) {
+				var separate = userInput.split("");
+					$("#output").text(separate);
+					console.log(separate);
+			
+			// } else if (userInput.charAt(0) !== vowel) {
+			// 		var separate = userInput.split();
+			// 		var vowelPlace = userInput.indexOf(vowels);
+			// 		var newWord = separate.splice(0,vowelPlace);
+			// 		alert(newWord)
+		
+			
 			}
 			});
-		// } else {
-		// 	$("#output").text(userInput);
 		
-		// } else(userInput.charAt(0) !== "a","e","i","o","u") {
-		// 		var separate = userInput.split();
-		// 		var move = separate.splice(userInput.length -1, 0, separate.charAt(0));
-		// 		$("#output").text(move);
-		// }
-
 	});
 });
