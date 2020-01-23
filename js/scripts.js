18f756f2dfedbacae8a982919a6f97f07a29142c
Student <example@example.com>
2020-01-23 10:41:28 -0800

    add vowels array to dry code

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,23 @@ $(document).ready(function(){
 	$("form").submit(function(event){
 		event.preventDefault();
 		var userInput = $("input#franklin").val();
-		
+	
 		var pigLatin = [];
 
-		if (userInput.charAt(0) === "a","e","i","o","u") {
+		var vowels = ["a", "e", "i", "o", "u"];
+			vowels.forEach(function(vowel) {
+			if (userInput.charAt(0) === vowel) {
 			 $("#output").text(userInput + "way");
-		}
+			}
+			});
+		// } else {
+		// 	$("#output").text(userInput);
+		
+		// } else(userInput.charAt(0) !== "a","e","i","o","u") {
+		// 		var separate = userInput.split();
+		// 		var move = separate.splice(userInput.length -1, 0, separate.charAt(0));
+		// 		$("#output").text(move);
+		// }
 
 	});
 });
